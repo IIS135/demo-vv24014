29fb20f7b4ab7a2b02557e5b323da69b4f922672
unknown <vv24014@ues.edu.sv>
2024-05-02 13:06:48 -0600

    Initial commit

---
+++ src/main/java/com/iis/vv24014/demo/vv24014/App.java
@@ -0,0 +1,18 @@
+package com.iis.vv24014.demo.vv24014;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String Hello() {
+    	return "Hello World";
+    }
+    
+    public static void main( String[] args)
+    {	
+    	App hw = new App();
+        System.out.println(hw.Hello() );
+    }
+}
---
+++ src/test/java/com/iis/vv24014/demo/vv24014/AppTest.java
@@ -0,0 +1,20 @@
+package com.iis.vv24014.demo.vv24014;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
